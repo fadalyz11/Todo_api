712f21a0e05d99566fd6eae021bfd38dd6176da8
fadalyz11 <fadalyz11@gmail.com>
2019-07-27 16:33:56 +0200

    final

--- public/app.js
+++ public/app.js
@@ -9,29 +9,65 @@ $(document).ready(function(){
             createTodo();
         }
     })
+
+    $('.list').on('click', 'li', function() {
+        updateTodo($(this))
+    })
+    $('.list').on('click','span', function(e) {
+      e.stopPropagation()
+     removeTodo($(this).parent())
+    })
 })
 
 function addTodos (todos) {
    todos.forEach(todo=> {
-    let newTodo = $('<li class = "task">'+ todo.name +'</li>')
-
+    addTodo(todo);
+   });
+}
+function addTodo (todo) {
+    let newTodo = $('<li class = "task">'+ todo.name +' <span>X</span></li>')
+        newTodo.data('id',todo._id)
+        newTodo.data('completed', todo.completed)
     if(todo.completed) {
         newTodo.addClass('done')
     }
     $('.list').append(newTodo)
-   });
+    
 }
 
 function createTodo () {
     let userInput = $('#todoInput').val();
-    
-    $.post('/api/todos', {name : userInput }).then(newTodo => {
-        console.log(newTodo)
+    $.post('/api/todos', {name : userInput })
+    .then(newTodo => {
+        $('#todoInput').val();
+        addTodo(newTodo)
     }).catch(err => {
         console.log(err)
     })
-    // .then(newTodo => {
-    //     console.log(newTodo)
-    //     .catch(err => console.log(err))
-    // })
+}
+
+function removeTodo(todo) {
+    let clickedId = todo.data('id')   
+    let deleteUrl  =  '/api/todos/' + clickedId
+    //  $(this).parent().remove();
+     $.ajax({
+         method:'DELETE',
+         url: deleteUrl
+     }).then(data => {
+        todo.remove() 
+        }).catch(err => {
+            console.log(err)
+        })
+}
+function updateTodo(todo) {
+    let updateUrl  =  '/api/todos/' + todo.data('id')  
+    let isDone  = !todo.data('completed')
+    let updatedData = {completed : isDone}
+ $.ajax({
+     method:'put',
+     url: updateUrl,
+     data: updatedData
+ }).then(updatedTodo => {
+     todo.toggleClass('done', isDone)
+ })
 }
